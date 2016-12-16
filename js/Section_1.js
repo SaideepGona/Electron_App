9473c842897288e530747556640e4535227fa224
SaideepGona <gona.saideep1@gmail.com>
2016-12-16 15:35:02 -0500

    Added an auto-updating time div. Also have some border shading for testing

--- js/Section_1.js
+++ js/Section_1.js
@@ -7,7 +7,30 @@
     // On-Ready sets the value of #version_num element equal to the current version number
 
   // Time
-    // Updates the #time element based on current time
-      var datetime = new Date();
-      console.log(datetime);
-      console.log('hello')
+    // Updates the #time element based on current time. Runs it at ready and periodically every xxx milliseconds
+    $(document).ready(updateClock);
+
+      function updateClock() {
+
+        var date = new Date();
+
+        var hours = date.getHours();
+
+        var ampm = 'AM'
+          if (hours >= 12) {
+            ampm = 'PM'
+          }
+
+          if ((hours % 12) == 0){
+            hours = 12
+          } else {
+            hours = hours % 12
+          }
+
+        var formatted_time = String(hours) + ':' + String(date.getMinutes()) + ' ' + ampm
+        console.log(formatted_time);
+
+        $("#time").text(formatted_time);
+
+      };
+      setInterval(updateClock, 10000);
