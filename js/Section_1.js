a880013406cd49339359c110af7dcc15d77272fe
SaideepGona <gona.saideep1@gmail.com>
2016-12-15 23:44:58 -0500

    Got the electron environment running. Heck ya.

---
+++ js/Section_1.js
@@ -0,0 +1,13 @@
+// This is the Javascript for Electron_App section 1 (#MCB1)
+
+  // Account Name
+    // On-Ready sets the  value of the #accountname element equal to the current account name
+
+  // Version No.
+    // On-Ready sets the value of #version_num element equal to the current version number
+
+  // Time
+    // Updates the #time element based on current time
+      var datetime = new Date();
+      console.log(datetime);
+      console.log('hello')
